1bb1b709a8aec0692897e0cbbd4e568763c6f53d
amolshirole <69356860+amolshirole@users.noreply.github.com>
2020-11-09 12:57:54 +0530

    Final Project

---
+++ src/components/pages/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../../HeroSection";
+import { homeObjFour } from "../HomePage/Data";
+import Pricing from "../Pricing/Pricing";
+
+const Products = () => {
+  return (
+    <>
+      <HeroSection {...homeObjFour} />
+    </>
+  );
+};
+
+export default Products;
---
+++ src/components/pages/SignUp/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../../HeroSection";
+import { homeObjThree } from "../HomePage/Data";
+import Pricing from "../Pricing/Pricing";
+
+const SignUp = () => {
+  return (
+    <>
+      <HeroSection {...homeObjThree} />
+    </>
+  );
+};
+
+export default SignUp;
